56ce7275fff9eedee9cbaed7df32e0715826a6b1
weiting <wt572051341@163.com>
2016-12-08 11:14:07 +0800

    标题不为空

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -14,8 +14,11 @@ class GroupsController < ApplicationController
 
 	def update
 		@group = Group.find(params[:id])
-		@group.update(group_params)
-		redirect_to groups_path, notice: "Update Success"
+		if @group.update(group_params)
+			redirect_to groups_path, notice: "Update Success"
+		else
+			render :edit
+		end
 	end
 
 	def edit
@@ -31,8 +34,11 @@ class GroupsController < ApplicationController
 
 	def create
 		@group = Group.new(group_params)
-		@group.save
-		redirect_to groups_path
+		if @group.save
+			redirect_to groups_path
+		else
+			render :new
+		end
 	end
 
 	private
