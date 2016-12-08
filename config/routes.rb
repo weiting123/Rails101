aeeb414900fb8c19b6cb8fce8415938a5c901565
weiting <wt572051341@163.com>
2016-12-08 17:05:49 +0800

    加入退出群组

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
 	resources :groups do
 		resources :posts
+		member do
+			post :join
+			post :quit
+		end
 	end
 	# root "welcome#index"
 	root "groups#index"
