7ed26125aff50ea0bd9e3bd2b2f2b3353c16044d
weiting <wt572051341@163.com>
2016-12-08 10:02:43 +0800

    02ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-	root "welcome#index"
+	resources :groups
+	# root "welcome#index"
+	root "groups#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
